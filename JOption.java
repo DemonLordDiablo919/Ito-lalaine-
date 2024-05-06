7d3965516c1a89772d467b6fb302cef802083f6c
DemonLordDiablo919 <137133638+DemonLordDiablo919@users.noreply.github.com>
2024-05-06 09:13:11 +0800

    Add files via upload

---
+++ JOption.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ACTIVITY;
+
+/**
+ *
+ * @author fadal
+ */
+class JOption {
+
+    static void showMessageDialog(Object object, String wrong_Username_or_Password, String message, int ERROR_MESSAGE) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
